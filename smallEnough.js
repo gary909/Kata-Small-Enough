cd32e57a0c8df8d69a3644078ad0269eab5ace2c
gary <gary.white@lodgit.com>
2021-06-03 10:27:43 +0200

    not yet working

---
+++ smallEnough.js
@@ -0,0 +1,26 @@
+// function smallEnough(a, limit){
+//     for (i = 0; i < a.length; i++) {
+//         return Math.max(a[i]); 
+//     }
+//   };
+
+  // function smallEnough(a, limit){
+  //     var x = new Boolean;
+  //   a.forEach(function(entry) {
+  //       if (a > limit){
+  //           x = false;
+  //           console.log(entry);
+  //       } else {
+  //           x = true;
+  //           console.log(entry);
+  //       }
+  //   })
+  // };
+
+
+// var b = [1, 2, 3];
+//     b.forEach(function(entry) {
+//     console.log(entry);
+// });
+
+console.log(smallEnough([66, 101], 200));
